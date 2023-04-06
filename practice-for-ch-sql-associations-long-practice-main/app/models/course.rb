a59bf69f188b02386f60d5f7c6c1330d20a909b5
Michele Zhang <michelezhang98@gmail.com>
2023-04-06 15:19:59 -0400

    Add associates for all models

--- practice-for-ch-sql-associations-long-practice-main/app/models/course.rb
+++ practice-for-ch-sql-associations-long-practice-main/app/models/course.rb
@@ -1,2 +1,28 @@
+# == Schema Information
+#
+# Table name: courses
+#
+#  id            :bigint           not null, primary key
+#  name          :string           not null
+#  prereq_id     :bigint
+#  instructor_id :bigint           not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
 class Course < ApplicationRecord
+    has_many :enrolled_students,
+        primary_key: :id,
+        foreign_key: :user_id,
+        class_name: :User
+        
+    has_many :enrollments,
+        primary_key: :id,
+        foreign_key: :enrollment_id,
+        class_name: :Enrollment
+    
+    has_one :prerequisite,
+        primary_key: :prereq_id,
+        foreign_key: :id,
+        class_name: :Course
+
 end
